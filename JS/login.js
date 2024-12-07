d5cd9c3d95a045c510fc9cda9c81453e6d5a2d86
nurnewas <nurnewasjr@gmail.com>
2024-12-07 11:08:58 -0600

    lock in

---
+++ JS/login.js
@@ -0,0 +1,19 @@
+console.log("connecting to Login server");
+
+document.getElementById("btn-login").addEventListener("click", function (e) {
+  e.preventDefault();
+  //   console.log("login successful");
+
+  //   get phone number
+  const phoneNumber = document.getElementById("phone-number").value;
+  const pinNumber = document.getElementById("pin-number").value;
+  //   console.log(phoneNumber, pinNumber);
+
+  if (phoneNumber === "01" && pinNumber === "1234") {
+    alert('Log in successful');
+  }
+  else{
+    alert('Invalid credentials');
+    return;
+  }
+});
