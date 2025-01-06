79b3e9e26d0eb52083b3427aa791e51c6cde1f03
nurnewas <nurnewasjr@gmail.com>
2025-01-06 09:16:40 -0600

    pin Number In home

--- JS/home.js
+++ JS/home.js
@@ -1,4 +1,18 @@
-document.getElementById("btn-add-Money").addEventListener("click", function (e) {
-  console.log("Btn clicked");
-  e.preventDefault();
-});
+document
+  .getElementById("btn-add-Money")
+  .addEventListener("click", function (e) {
+    console.log("Btn clicked");
+    e.preventDefault();
+
+    //   Get Money Added to the Account
+    const addMoneyInput = document.getElementById("input-add-money");
+    // const moneyAdded = parseFloat(addMoneyInput.value);
+    console.log(addMoneyInput.value);
+
+
+    // pin Number
+    const pinNumberInput = document.getElementById("input-pin-number");
+    const pinNumber = pinNumberInput.value;
+    console.log(pinNumber);
+    
+  });
