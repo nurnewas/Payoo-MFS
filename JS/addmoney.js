0fc3fe72c6cbc3cc11cd6f57e691e6bc46d460a6
nurnewas <nurnewasjr@gmail.com>
2025-01-10 20:44:40 -0600

    r add money dainamic

--- JS/addmoney.js
+++ JS/addmoney.js
@@ -1,10 +1,26 @@
 // Add listener in Btn
-document.getElementById("btn-add-Money")
+document
+  .getElementById("btn-add-Money")
   .addEventListener("click", function (e) {
     e.preventDefault();
 
     // Add Money and Pin Form Input
     const addMoney = document.getElementById("input-add-money").value;
+    const addMoneyNumber = parseFloat(addMoney);
     const pinNumber = document.getElementById("input-pin-number").value;
     console.log(addMoney, pinNumber);
+
+    // pin verify
+    if (parseInt(pinNumber) === 1234) {
+      // add money to the account
+      const balance = document.getElementById("account-balance").innerText;
+      console.log(balance);
+      const balanceNumber = parseFloat(balance);
+
+      // update the balance
+      const newBalance = balanceNumber + addMoneyNumber;
+      document.getElementById("account-balance").innerText = newBalance;
+    } else {
+      alert("Invalid Pin Number");
+    }
   });
